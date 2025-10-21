d1bda53e984412b58c6da1d9c7dd758f927cd7c0
Kevalg-Solanki <kevalg1950@gmail.com>
2025-10-21 16:08:23 +0530

    feat(auth): update login functionality

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -13,15 +13,25 @@ const authRouter = require("express").Router();
 
 //APIs
 
+//**Registration Api**
 //step 1
 authRouter.post("/verify-credential",validateRequest(verifyCredentialSchema),verifyCredentialAndSendOtp
 );
 
+//step 2
 authRouter.post("/verify-otp-register",validateRequest(verifyOtpRegisterSchema),verifyOtpForRegistration)
 
+//step 3
 authRouter.post("/register",validateRequest(registrationSchema),register);
 
+
+//**Login**
 authRouter.post("/login",validateRequest(loginSchema),login);
+
+
+
+
+
 
 module.exports = authRouter;
 
